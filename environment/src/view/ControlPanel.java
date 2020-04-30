91bef4ca5234ff4b15f1a86f5c85698d49c449a5
Levente Bajczi <bajczilevi@gmail.com>
2020-04-30 22:59:31 +0200

    Implemented file chooser and added buttons to control panel

--- environment/src/view/ControlPanel.java
+++ environment/src/view/ControlPanel.java
@@ -2,14 +2,58 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class ControlPanel extends JPanel {
+    private JLabel lFileName;
     private JTextField fileName;
     private JButton browse;
     private JButton load;
     private JButton start;
     private JButton stop;
     public ControlPanel() {
-        this.setBackground(Color.RED);
+        lFileName = new JLabel("File: ");
+        this.add(lFileName);
+
+        fileName = new JTextField();
+        fileName.setColumns(20);
+        this.add(fileName);
+
+        browse = new JButton("Browse");
+        browse.addActionListener(this::browse);
+        this.add(browse);
+
+        load = new JButton("Load");
+        load.addActionListener(this::load);
+        this.add(load);
+
+        start = new JButton("Start");
+        start.addActionListener(this::start);
+        this.add(start);
+
+        stop = new JButton("Stop");
+        stop.addActionListener(this::stop);
+        this.add(stop);
+    }
+
+    private void stop(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    private void start(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    private void browse(ActionEvent actionEvent) {
+        JFileChooser jFileChooser = new JFileChooser();
+        File workingDir = new File(System.getProperty("user.dir"));
+        jFileChooser.setCurrentDirectory(workingDir);
+        int res = jFileChooser.showOpenDialog(this);
+        if (res == JFileChooser.APPROVE_OPTION) fileName.setText(workingDir.toURI().relativize(jFileChooser.getSelectedFile().toURI()).getPath());
+    }
+
+    private void load(ActionEvent actionEvent) {
+        //TODO
     }
 }
